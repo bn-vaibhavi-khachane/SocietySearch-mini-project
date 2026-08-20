21d929ce80a04af345a84df25368cf02ffa31c1a
bn-vaibhavi-khachane <Vaibhavi.Khachane@Bridgenext.com>
2026-08-20 17:42:59 +0530

    adds exception handling and validations

--- SocietySearch.Server/Model/DTO/RegisterManagerRequestDto.cs
+++ SocietySearch.Server/Model/DTO/RegisterManagerRequestDto.cs
@@ -1,10 +1,19 @@
-﻿namespace SocietySearch.Server.Model.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocietySearch.Server.Model.DTO
 {
     public class RegisterManagerRequestDto
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+
+        [Required, MinLength(8)]
         public string Password { get; set; } = string.Empty;
+
+        [Required]
         public string Name { get; set; } = string.Empty;
+
+        [Required, Phone]
         public string PhoneNumber { get; set; } = string.Empty;
     }
 }
--- SocietySearch.Server/Model/DTO/UpdateSocietyRequestDto.cs
+++ SocietySearch.Server/Model/DTO/UpdateSocietyRequestDto.cs
@@ -1,14 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SocietySearch.Server.Model.DTO
 {
     public class UpdateSocietyRequestDto
     {
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public string Summary { get; set; }
-        public string EstablishmentYear { get; set; }
-        public string SocietyImageUrl { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Address { get; set; } = string.Empty;
+
+        [Required]
+        public string Summary { get; set; } = string.Empty;
+
+        [Required, RegularExpression(@"^(19|20)\d{2}$", ErrorMessage = "EstablishmentYear must be a valid four-digit year.")]
+        public string EstablishmentYear { get; set; } = string.Empty;
+
+        [Required, Url]
+        public string SocietyImageUrl { get; set; } = string.Empty;
 
         // Ids of amenities associated with this society, stored as JSON
-        public List<Guid?> AmenityIds { get; set; }
+        public List<Guid?> AmenityIds { get; set; } = new();
     }
 }
